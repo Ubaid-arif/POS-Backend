b55ad532be5bd1e1443769bb4711835b5be24bc3
Sheryar-Rajput <Sheryarrajput47@gmail.com>
2023-11-06 01:34:40 +0500

    changes

--- controller/sales.js
+++ controller/sales.js
@@ -1,10 +1,19 @@
 const { createSales, getAllSale, updateSale } = require("../service/sales");
 
-const create_sales = (req, res) => {};
+const create_sales = async (req, res) => {
+  const result = await createSales(req.data);
+  return res.status(200).json({ result });
+};
 
-const get_sales = (req, res) => {};
+const get_sales = async (req, res) => {
+  const result = await getAllSale(req.query);
+  return res.status(200).json({ result });
+};
 
-const update_sales = (req, res) => {};
+const update_sales = async (req, res) => {
+  const result = await updateSale(req.data);
+  return res.status(200).json({ result });
+};
 
 module.exports = {
   create_sales,
